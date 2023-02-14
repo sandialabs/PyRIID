15105f04b7e5df1b345ff3f0699803feeff0333d
Leon Ross <lcross@sandia.gov>
2023-02-14 15:53:54 -0700

    __eq__ and tests

--- riid/data/sampleset.py
+++ riid/data/sampleset.py
@@ -137,6 +137,9 @@ class SampleSet():
     def __repr__(self):
         return self.__str__()
 
+    def __eq__(self, ss: SampleSet):
+        return np.array_equal(self._spectra.values, ss._spectra.values)
+
     # region Properties
 
     @property
