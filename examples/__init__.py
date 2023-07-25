2d0ed6b67752e6ec51f44483a2b9ed9d2cb8261f
anbusto <anbusto@sandia.gov>
2023-07-25 13:37:39 -0600

    added init file for examples

---
+++ examples/__init__.py
@@ -0,0 +1,4 @@
+# Copyright 2021 National Technology & Engineering Solutions of Sandia, LLC (NTESS).
+# Under the terms of Contract DE-NA0003525 with NTESS,
+# the U.S. Government retains certain rights in this software.
+"""This module is of examples of how to use PyRIID"""
