996537d09884f4e47dc57879b9f433f3428fef75
Jasmine Lau <jvlau@sandia.gov>
2023-08-02 10:58:10 -0600

    detect json made

--- riid/cli/main.py
+++ riid/cli/main.py
@@ -42,8 +42,8 @@ def train(model_type, data_path, model_path=None, results_dir_path=None):
 def identify(model_path, data_path, results_dir_path=None):
     from riid.models.neural_nets import MLPClassifier
 
-    print(f"Identifying measurements with model: {model_path}")
-    print(f"                               data: {data_path}")
+    print(f"Identifying measurements with model:        {model_path}")
+    print(f"                               data:        {data_path}")
     if not results_dir_path:
         results_dir_path = "./identify_results/"
     if not os.path.exists(results_dir_path):
@@ -68,12 +68,14 @@ def identify(model_path, data_path, results_dir_path=None):
               type=click.Path(exists=False, file_okay=True),
               help="Path to directory where identification results are output")
 def detect(gross_path, bg_path, results_dir_path=None):
+    import json
+
     import numpy as np
 
     from riid.anomaly import PoissonNChannelEventDetector
 
-    print(f"Detecting events with gross measurements:      {gross_path}")
-    print(f"                      background measurements: {bg_path}")
+    print(f"Detecting events with gross measurements:       {gross_path}")
+    print(f"                 background measurements:       {bg_path}")
     if not results_dir_path:
         results_dir_path = "./detect_results/"
     if not os.path.exists(results_dir_path):
@@ -148,6 +150,29 @@ def detect(gross_path, bg_path, results_dir_path=None):
         first_measurement_id = measurement_ids[0]
         last_measurement_id = measurement_ids[-1]
         print(f"  > {event_duration:.2f}s from {first_measurement_id} to {last_measurement_id}")
+
+    event_result_keys = ["gross_spectrum", "bg_spectrum", "duration_seconds", "measurement_ids"]
+    event_result_pairs = {}
+    for key, value in zip(event_result_keys, event_result):
+        if key == "measurement_ids":
+            event_result_pairs[key] = list(value)
+        else:
+            event_result_pairs[key] = value.tolist()
+
+    # detect_results = json.dumps(event_result_pairs)
+    with open(f"{results_dir_path}/detect.json", "w+") as outfile:
+        json.dump(event_result_pairs, outfile, indent=4)
+        # outfile.write("{\n\t")
+        # for i in event_result_pairs:
+        #     outfile.write(json.dumps(i))
+        #     outfile.write(': ')
+        #     outfile.write(json.dumps(event_result_pairs[i]))
+        #     if i == "measurement_ids":
+        #         outfile.write('\n')
+        #     else:
+        #         outfile.write(', \n\t')
+        # outfile.write("}")
+    outfile.close
 
     """ TODO: Save results to JSON file with following structure:
     [
